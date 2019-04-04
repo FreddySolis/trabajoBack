0ebea8e7397d0cc2d0ab2c57f57836530b348f1c
Freddy solis <173239@ids.upchiapas.edu.mx>
2019-04-03 19:06:28 -0600

    Migraciones

--- database/migrations/2019_03_09_201549_tarjeta.php
+++ database/migrations/2019_03_09_201549_tarjeta.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Tarjeta extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tarjeta', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('score');
+            $table->integer('id_usuario');
+            $table->integer('numero');
+            $table->string('tipo');
+            $table->string('banco');
+            $table->integer('cvv');
+            $table->string('expiración');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tarjeta');
     }
 }
--- database/migrations/2019_03_09_201616_producto.php
+++ database/migrations/2019_03_09_201616_producto.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Producto extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('productos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('score');
+            $table->string('producto');
+            $table->string('descripcion');
+            $table->integer('precio');
+            $table->string('imagen');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('productos');
     }
 }
